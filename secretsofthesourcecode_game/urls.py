3491de5bda690cac7eb1841a3f9d7db754e3da37
Esther Kariuki <esywacu@gmail.com>
2025-02-03 13:01:31 +0000

    add lose

--- secretsofthesourcecode_game/urls.py
+++ secretsofthesourcecode_game/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.start_game, name='start_game'),
     path('accuse/', views.make_accusation, name='make_accusation'),
+    path('lose/', views.lose, name='lose'),
 ]
